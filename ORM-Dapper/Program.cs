5f28fe391da8ea0d1c52bfef08cd1085236dd4ef
petekal1323 <petekal1323@gmail.com>
2025-07-15 00:32:28 -0400

    get all departments method

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.IO;
 using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
+using ORM_Dapper;
 
 var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -11,4 +12,12 @@ var config = new ConfigurationBuilder()
 
 string connString = config.GetConnectionString("DefaultConnection"); 
 IDbConnection conn = new MySqlConnection(connString);
-       
+
+var dapdepartmentRepo = new DapperDepartmentRepository(conn);
+
+var departments = dapdepartmentRepo.GetAllDepartments();
+
+foreach (var dep in departments)
+{
+    Console.WriteLine($"Name: {dep.Name} | Department ID: {dep.DepartmentID}");
+}
